56fe595755cd40692de1416527bb11f0ff92f81a
Sreeparvathi K <sreeparvathik97@gmail.com>
2019-02-28 20:29:26 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Twimedia</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body class="landing">
+
+		<!-- Header -->
+			<header id="header" class="alt">
+				<h1><a href="index.php">Twimedia</a></h1>
+				<a href="#nav">Menu</a>
+			</header>
+
+		<!-- Nav -->
+			<nav id="nav">
+				<ul class="links">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="search_topic.php">Search by Topic</a></li>
+					<li><a href="search_category.php">Search by Category</a></li>
+				</ul>
+			</nav>
+
+		<!-- Banner -->
+			<section id="banner">
+				<i class="icon fa-diamond"></i>
+				<h2>Twimedia</h2>
+
+			</section>
+
+		<!-- One -->
+			<section id="one" class="wrapper style1">
+				<div class="inner">
+					<article class="feature left">
+						<span class="image"><img src="images/pic01.jpg" alt="" /></span>
+						<div class="content">
+							<h2>Enter a <b style="color:red">Topic</b> to get related top news and Tweets of your friends</p>
+							<ul class="actions">
+								<li>
+									<a href="search_topic.php" class="button alt">Go</a>
+								</li>
+							</ul>
+						</div>
+					</article>
+					<article class="feature right">
+						<span class="image"><img src="images/pic02.jpg" alt="" /></span>
+						<div class="content">
+							<h2>Enter a <b style="color:red">Category</b> to get related top news and Tweets of your friends</p>
+							<ul class="actions">
+								<li>
+									<a href="search_category.php" class="button alt">Go</a>
+								</li>
+							</ul>
+						</div>
+					</article>
+				</div>
+			</section>
+
+
+
+
+
+
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</php>
---
+++ search_topic.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Twimedia</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" /> 
+		<link rel="stylesheet" href="assets/css/main.css" />
+
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<h1><a href="index.php">Twimedia</a></h1>
+				<a href="#nav">Menu</a>
+			</header>
+
+		<!-- Nav -->
+			<nav id="nav">
+				<ul class="links">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="search_topic.php">Search By Topic</a></li>
+					<li><a href="search_category.php">Search By Category</a></li>
+				</ul>
+			</nav>
+
+		<!-- Main -->
+			<section id="main" class="wrapper">
+				<div class="container">
+
+					<header class="major special">
+						<h2>Search by Topic</h2>
+					</header>
+
+
+
+				</div>
+			</section>
+
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
